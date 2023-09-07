20ea2f1643007b36e97dd90b6298b1ab6e288e3c
JizzyXO <116279066+JizzyXO@users.noreply.github.com>
2023-09-07 14:29:50 +0300

    Add files via upload

---
+++ MyMathKatelkinTaskTwo/src/main/java/org/example/TrigonometryAndPrimes.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class TrigonometryAndPrimes {
+    public static double cotangent(double angleInRadians) {
+        return 1.0 / Math.tan(angleInRadians);
+    }
+
+    public static double arccotangent(double value) {
+        return Math.atan(1.0 / value);
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printPrimesInRange(int start, int end) {
+        System.out.println("Простые числа в диапазоне от " + start + " до " + end + ":");
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+}
